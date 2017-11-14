64a1cceef56f6103048af5580e8bdbb3d3eb1ae7
yangxue <yangxue08@baidu.com>
2017-11-14 11:57:09 +0800

    分页组件开发

--- src/component/page/page.ts
+++ src/component/page/page.ts
@@ -14,7 +14,13 @@ import {
     }
 })
 export class PageComponent implements OnInit {
-    pageSize = 6;
+    // 每页20条
+    count = 20;
+    // 页数
+    pageSize = [1,2,3,4,5,6,7,8,9,10,11];
+    // 前4页
+    firstPages = [1,2,3,4];
+    // 当前页
     currrentIndex = 1;
     lastIndex = 1;
 
@@ -26,10 +32,10 @@ export class PageComponent implements OnInit {
 
     }
     jumpTo (index: number) {
-        if (this.currrentIndex > 0 && this.currrentIndex < this.pageSize + 1) {
+        if (this.currrentIndex > 0 && this.currrentIndex < this.pageSize.length + 1) {
             if (+index === -2 && this.currrentIndex !== 1) {
                 this.currrentIndex--;
-            } else if (+index === -1 && this.currrentIndex !== this.pageSize) {
+            } else if (+index === -1 && this.currrentIndex !== this.pageSize.length) {
                 this.currrentIndex++;
             } else if (+index > -1) {
                 this.currrentIndex = index;
