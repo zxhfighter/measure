24066f2a6d82bffd2faa3b19d99d4586815770d5
yangxue <yangxue08@baidu.com>
2017-11-20 15:03:23 +0800

    增加select组件

--- src/component/page/page.module.ts
+++ src/component/page/page.module.ts
@@ -1,9 +1,10 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {PageComponent} from './page';
+import {SelectModule} from '../select';   
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [CommonModule, SelectModule],
     declarations: [PageComponent],
     exports: [PageComponent]
 })
--- src/component/page/page.ts
+++ src/component/page/page.ts
@@ -2,6 +2,7 @@ import {
     Component, Input, Output, EventEmitter,
     OnInit, ViewEncapsulation, ChangeDetectionStrategy
 } from '@angular/core';
+import {SelectConfig} from "../select/select.config";
 
 @Component({
     selector: 'nb-page',
@@ -18,11 +19,33 @@ export class PageComponent implements OnInit {
     count = 20;
     // 页数
     pageSize = [1,2,3,4,5,6,7,8,9,10,11];
+    // 每页显示条数可选列表
+    list = [10, 20, 30, 50, 100];
     // 前4页
     firstPages = [1,2,3,4];
     // 当前页
     currrentIndex = 1;
     lastIndex = 1;
+
+    protected selectedData1: SelectConfig;
+    protected selectData: SelectConfig[] = [
+        {
+            label: '10',
+            value: 10
+        },
+        {
+            label: '20',
+            value: 20
+        },
+        {
+            label: '30',
+            value: 30
+        },
+        {
+            label: '50',
+            value: 50
+        }
+    ];
 
     constructor() {
 
