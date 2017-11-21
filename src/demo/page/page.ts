fb2136790695270de34637b926a7460f47856c6f
yangxue <yangxue08@baidu.com>
2017-11-21 15:29:16 +0800

    增加可选参数，每页展示页数的列表

--- src/demo/page/page.ts
+++ src/demo/page/page.ts
@@ -11,11 +11,22 @@ import {
 })
 export class DemoPage implements OnInit {
 
+    // pageSize = [1,2,3,4,5,6,7,8,9,10,11];
+    // 每页显示多少条
+    count = 30;
+    // 一共多少条
+    total = 302;
+    // 可选择的每页显示条数
+    list = [10, 20];
+
     constructor() {
 
     }
 
     ngOnInit() {
 
+    }
+    pageChange(event) {
+        console.log(event);
     }
 }
