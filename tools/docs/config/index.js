effb031453524532d3b95a61dfc7b32f8f24860d
zxhfighter <369749456@qq.com>
2017-11-23 18:15:32 +0800

    fix doc build problem

--- tools/docs/config/index.js
+++ tools/docs/config/index.js
@@ -3,8 +3,9 @@ const Dgeni = require('dgeni');
 const DgeniPackage = Dgeni.Package;
 
 const projectRootDir = path.resolve(__dirname, '../../..');
-const sourceDir = path.resolve(projectRootDir, 'src/component');
+const sourceDir = path.resolve(projectRootDir, 'src/');
 const outputDir = path.resolve(projectRootDir, 'dist/docs/api');
+const templateDir = path.resolve(__dirname, './templates');
 
 let apiDocsPackage =
 
@@ -14,79 +15,81 @@ let apiDocsPackage =
         require('dgeni-packages/typescript')
     ])
 
-// 引入一序列需要处理文档处理器
-// .processor(require('./processors/link-inherited-docs'))
-.processor(require('./processors/docs-private-filter'))
-.processor(require('./processors/categorizer'))
-.processor(require('./processors/component-grouper'))
-// 设置源文件和输出路径
-.config(function(log, readFilesProcessor, writeFilesProcessor) {
-    // 设置日志等级
-    log.level = 'info';
+    // 引入一序列需要处理文档处理器
+    // .processor(require('./processors/link-inherited-docs'))
+    .processor(require('./processors/docs-private-filter'))
+    .processor(require('./processors/categorizer'))
+    .processor(require('./processors/component-grouper'))
+    // 设置源文件和输出路径
+    .config(function (log, readFilesProcessor, writeFilesProcessor) {
+        // 设置日志等级
+        log.level = 'info';
 
-    // 设置项目根目录为基准路径
-    readFilesProcessor.basePath = sourceDir;
+        // 设置项目根目录为基准路径
+        readFilesProcessor.basePath = sourceDir;
 
-    // disable for now as we are using readTypeScriptModules
-    readFilesProcessor.$enabled = false;
+        // disable for now as we are using readTypeScriptModules
+        readFilesProcessor.$enabled = false;
 
-    // 指定输出路径
-    writeFilesProcessor.outputFolder = outputDir;
-})
+        // 指定输出路径
+        writeFilesProcessor.outputFolder = outputDir;
+    })
 
-// 设置输出文件风格
-.config(function(computePathsProcessor) {
-    computePathsProcessor.pathTemplates = [{
-        docTypes: ['componentGroup'],
-        pathTemplate: '${name}',
-        outputPathTemplate: '${name}.html',
-    }];
-})
+    // 设置输出文件风格
+    .config(function (computePathsProcessor) {
+        computePathsProcessor.pathTemplates = [{
+            docTypes: ['componentGroup'],
+            pathTemplate: '${name}',
+            outputPathTemplate: '${name}.html',
+        }];
+    })
 
-.config(function(parseTagsProcessor) {
-    parseTagsProcessor.tagDefinitions = parseTagsProcessor.tagDefinitions.concat([
-        { name: 'docs-private' },
-        { name: 'type' },
-        { name: 'default' },
-        { name: 'readonly' }
-    ]);
-})
+    .config(function (parseTagsProcessor) {
+        parseTagsProcessor.tagDefinitions = parseTagsProcessor.tagDefinitions.concat([
+            { name: 'docs-private' },
+            { name: 'type' },
+            { name: 'default' },
+            { name: 'readonly' }
+        ]);
+    })
 
-// TypeScript 处理器
-.config(function(readTypeScriptModules) {
-    // ts文件基准文件夹
-    readTypeScriptModules.basePath = sourceDir;
-    // 隐藏private变量
-    readTypeScriptModules.hidePrivateMembers = true;
-    // typescript 入口
-    readTypeScriptModules.sourceFiles = [
-        '*/**/index.ts'
-    ];
-})
+    // TypeScript 处理器
+    .config(function (readTypeScriptModules) {
+        // ts文件基准文件夹
+        readTypeScriptModules.basePath = sourceDir;
 
-// 设置模板引擎
-.config(function(templateFinder, templateEngine) {
-    // 指定模板文件路径
-    templateFinder.templateFolders = [path.resolve(__dirname, './templates')];
-    // 设置文件类型与模板之间的匹配关系
-    templateFinder.templatePatterns = [
-        '${ doc.template }',
-        '${ doc.id }.${ doc.docType }.template.html',
-        '${ doc.id }.template.html',
-        '${ doc.docType }.template.html',
-        '${ doc.id }.${ doc.docType }.template.js',
-        '${ doc.id }.template.js',
-        '${ doc.docType }.template.js',
-        '${ doc.id }.${ doc.docType }.template.json',
-        '${ doc.id }.template.json',
-        '${ doc.docType }.template.json',
-        'common.template.html'
-    ];
-    // Nunjucks模板引擎，默认的标识会与Angular冲突
-    templateEngine.config.tags = {
-        variableStart: '{$',
-        variableEnd: '$}'
-    };
-});
+        // 隐藏private变量
+        readTypeScriptModules.hidePrivateMembers = true;
+
+        // typescript 入口
+        readTypeScriptModules.sourceFiles = [
+            'component/**/index.ts'
+        ];
+    })
+
+    // 设置模板引擎
+    .config(function (templateFinder, templateEngine) {
+        // 指定模板文件路径
+        templateFinder.templateFolders = [templateDir];
+        // 设置文件类型与模板之间的匹配关系
+        templateFinder.templatePatterns = [
+            '${ doc.template }',
+            '${ doc.id }.${ doc.docType }.template.html',
+            '${ doc.id }.template.html',
+            '${ doc.docType }.template.html',
+            '${ doc.id }.${ doc.docType }.template.js',
+            '${ doc.id }.template.js',
+            '${ doc.docType }.template.js',
+            '${ doc.id }.${ doc.docType }.template.json',
+            '${ doc.id }.template.json',
+            '${ doc.docType }.template.json',
+            'common.template.html'
+        ];
+        // Nunjucks模板引擎，默认的标识会与Angular冲突
+        templateEngine.config.tags = {
+            variableStart: '{$',
+            variableEnd: '$}'
+        };
+    });
 
 module.exports = apiDocsPackage;
