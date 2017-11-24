15cc3f119034d202fdde933b5976d5cc905662ff
zxhfighter <369749456@qq.com>
2017-11-24 15:39:48 +0800

    api for carousel component

--- src/demo/carousel/auto/carousel-auto.ts
+++ src/demo/carousel/auto/carousel-auto.ts
@@ -6,4 +6,10 @@ import {Component, ViewEncapsulation} from '@angular/core';
     styleUrls: ['./carousel-auto.less'],
     encapsulation: ViewEncapsulation.None
 })
-export class DemoCarouselAuto {}
+export class DemoCarouselAuto {
+    datasource = [
+        {title: 'apple', imageUrl: 'https://images.unsplash.com/reserve/bIdO4DDS4qwVF6pHN4qr__MG_1605.jpg?dpr=2&auto=format&fit=crop&w=568&h=379&q=60&cs=tinysrgb&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D'},
+        {title: 'people', imageUrl: 'https://images.unsplash.com/photo-1500440853933-3796d0182c96?dpr=2&auto=format&fit=crop&w=568&h=426&q=60&cs=tinysrgb&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D'},
+        {title: 'star', imageUrl: 'https://images.unsplash.com/photo-1488485300416-de7f8f876d4b?dpr=2&auto=format&fit=crop&w=568&h=379&q=60&cs=tinysrgb&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D'}
+    ];
+}
