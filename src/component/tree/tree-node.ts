a9bf9538a91818d54ae724a7f347f7562494fad2
wangsong09 <wangsong09@baidu.com>
2017-11-29 15:00:50 +0800

    add a component tree

---
+++ src/component/tree/tree-node.ts
@@ -0,0 +1,44 @@
+import {
+    Component, 
+    Input, 
+    Output, 
+    EventEmitter,
+    OnInit, 
+    ViewEncapsulation, 
+    ChangeDetectionStrategy, 
+    Inject, 
+    forwardRef
+} from '@angular/core';
+import {TreeNode} from './treenode';
+import {TreeComponent} from './tree';
+
+@Component({
+    selector: 'nb-tree-node',
+    templateUrl: './tree-node.html',
+    encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.Default,
+    preserveWhitespaces: false,
+    host: {
+        'class': 'nb-widget nb-tree-node'
+    }
+})
+export class TreeNodeComponent implements OnInit {
+
+     /** input tree-node value */
+    @Input() node: TreeNode;
+
+    constructor(@Inject(forwardRef(() => TreeComponent)) public tree: TreeComponent) { }
+
+    ngOnInit() { }
+    
+    /** listen tree-node click event */
+    onNodeClick(event: MouseEvent) {
+        this.tree.onNodeClick(event, this.node);
+    }
+
+    /** fold or unfold tree-node event */
+    toggle(event: Event) {
+        event.stopPropagation();
+        this.node.isExpanded = !this.node.isExpanded;
+    }
+}
---
+++ src/component/tree/treenodeparent.ts
@@ -0,0 +1,3 @@
+export interface TreeNodeParent {
+    id: string;
+}
