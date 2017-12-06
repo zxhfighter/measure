1d01c7d66855a49c4964fa77d28dc9ac6c9f15e5
wangfengjiao <wangfengjiao01@baidu.com>
2017-12-06 15:28:23 +0800

    accordion component finished

--- src/component/accordion/accordion.ts
+++ src/component/accordion/accordion.ts
@@ -2,6 +2,7 @@ import {
     Component, Input, Output, EventEmitter,
     OnInit, ViewEncapsulation, ChangeDetectionStrategy
 } from '@angular/core';
+import { Panel } from './panel';
 
 @Component({
     selector: 'nb-accordion',
@@ -15,6 +16,55 @@ import {
     exportAs: 'nbAccordion'
 })
 export class AccordionComponent implements OnInit {
+
+    /**
+     * 渲染对象
+     *
+     * @type {object}
+     */
+    panels: Panel[];
+
+    @Input() set datasource(data: Panel[]) {
+        this.panels = data;
+    }
+
+    get datasource(): Panel[] {
+        return this.panels;
+    }
+
+    /**
+     * 激活的panel下标, 如果为-1视为全部折叠
+     *
+     */
+    @Input() activeIndex: number = 0;
+
+    /**
+     * 是否hover展开
+     *
+     */
+    @Input() hoverable: boolean = false;
+
+    /**
+     * 折叠方式
+     * false - 互斥折叠
+     * true - 全部可折叠
+     *
+     */
+    @Input() collapsible: boolean = false;
+
+    /**
+     * 正常状态三角图标的样式
+     * TODO 项目中确定iconfont方案后完善
+     *
+     */
+    headerIcon: string = 'caret-right';
+
+    /**
+     * 展开状态三角图标的样式
+     *
+     */
+    activeHeaderIcon: string = 'caret-down';
+
     constructor() {
 
     }
@@ -22,4 +72,39 @@ export class AccordionComponent implements OnInit {
     ngOnInit() {
 
     }
+
+    /**
+     * 变更索引
+     *
+     * @param {number} activeIndex - 指定索引
+     */
+    changeActiveIndex(activeIndex: number) {
+        this.activeIndex = activeIndex;
+    }
+
+    /**
+     * 激活指定panel
+     *
+     * @param {number} activeIndex - 指定索引
+     */
+    onClickPanel(activeIndex: number) {
+        // 非互斥折叠
+        if (this.collapsible) {
+            // 该元素内容已展开，折叠收缩
+            if (this.activeIndex === activeIndex) {
+                activeIndex = -1;
+            }
+        }
+        // 互斥折叠
+        else {
+            // 该元素内容已展开，什么都不做
+            if (this.activeIndex === activeIndex) {
+                return;
+            }
+        }
+        // 激活当前元素
+        this.changeActiveIndex(activeIndex);
+    }
 }
+
+
--- src/demo/accordion/accordion.ts
+++ src/demo/accordion/accordion.ts
@@ -1,6 +1,7 @@
 import {
     Component, OnInit, ChangeDetectionStrategy
 } from '@angular/core';
+import { Panel } from '../../component/accordion/panel';
 
 @Component({
     selector: 'demo-accordion',
@@ -10,6 +11,33 @@ import {
     changeDetection: ChangeDetectionStrategy.Default
 })
 export class DemoAccordion implements OnInit {
+
+    collapsible: boolean = true;
+
+    hoverable: boolean = true;
+
+    panels: Panel[] = [
+        {
+            header: '1.一级条目',
+            content: `
+                Mauris mauris ante, blandit et, ultrices a, suscipit eget,
+                quam. Integer ut neque. Vivamus nisi metus, molestie vel,
+                gravida in, condimentum sit amet, nunc. Nam a nibh. Donec
+                suscipit eros. Nam mi. Proin viverra leo ut odio. Curabitur
+                malesuada. Vestibulum a velit eu ante scelerisque vulputate.
+            `
+        },
+        {
+            header: '2.一级条目',
+            content: `
+                Mauris mauris ante, blandit et, ultrices a, suscipit eget,
+                quam. Integer ut neque. Vivamus nisi metus, molestie vel,
+                gravida in, condimentum sit amet, nunc. Nam a nibh. Donec
+                suscipit eros. Nam mi. Proin viverra leo ut odio. Curabitur
+                malesuada. Vestibulum a velit eu ante scelerisque vulputate.
+            `
+        }
+    ];
 
     constructor() {
 
