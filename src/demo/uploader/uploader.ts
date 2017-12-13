2ffadcf2f4eaadda47b4434ea9cdf0fbd2cc53e6
wangfengjiao <wangfengjiao01@baidu.com>
2017-12-13 18:21:00 +0800

    uploader component finished

--- src/demo/uploader/uploader.ts
+++ src/demo/uploader/uploader.ts
@@ -15,34 +15,51 @@ export class DemoUploader implements OnInit {
     uploadedFiles: any[] = [
         {
             url: 'https://www.baidu.com/img/bd_logo1.png',
+            fileUid: 123456,
             name: 'bd_logo1.png',
             size: 7877,
             type: 'image/jpeg'
         },
         {
             url: 'http://nodejs.cn/static/images/logo.svg',
+            fileUid: 234567,
             name: 'logo.svg',
             size: 5925,
             type: 'image/svg+xml'
         }
     ];
 
-    constructor() {
+    uploadedFilesList: any[] = [];
 
+    constructor() {
+        for (let i = 0; i < 4; i++) {
+            // 深拷贝，避免四个例子的数据源共享
+            this.uploadedFilesList.push(this.uploadedFiles.concat());
+        }
     }
 
     ngOnInit() {
 
     }
 
-    onBasicUpload(event) {
-        console.log(1);
-        let responseObject = JSON.parse(event.xhr.response);
-
-        // TODO 应该优化下新上传的才刷新
-        this.uploadedFiles = this.uploadedFiles.concat(
-            responseObject.data.files
-        );
-        // alert('上传成功');
+    onImageUpload(event) {
+        const responseObject = JSON.parse(event.xhr.response);
+        // event.file = responseObject.data.files[0];
+        const file = event.file;
+        file.url = responseObject.data.files[0].url;
     }
+
+    onImageBarUpload(event) {
+        const responseObject = JSON.parse(event.xhr.response);
+        const file = event.file;
+        file.url = responseObject.data.files[0].url;
+    }
+
+    onFileUpload(event) {
+    }
+
+    onFileBarUpload(event){
+
+    }
+
 }
