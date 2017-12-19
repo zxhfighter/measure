b502e88475a3e48f156377404b282272425aa5f2
wangfengjiao <wangfengjiao01@baidu.com>
2017-12-20 00:20:26 +0800

    accordion demo finished

--- src/demo/accordion/basic/accordion-basic.ts
+++ src/demo/accordion/basic/accordion-basic.ts
@@ -1,8 +1,36 @@
 import { Component } from '@angular/core';
+import { Panel } from '../../../component/accordion/panel';
 
 @Component({
     selector: 'demo-accordion-basic',
     templateUrl: './accordion-basic.html',
     styleUrls: ['./accordion-basic.less']
 })
-export class AccordionBasicDemo { }
+export class AccordionBasicDemo {
+    collapsible: boolean = true;
+
+    hoverable: boolean = true;
+
+    panels: Panel[] = [
+        {
+            header: '1.一级条目',
+            content: `
+                Mauris mauris ante, blandit et, ultrices a, suscipit eget,
+                quam. Integer ut neque. Vivamus nisi metus, molestie vel,
+                gravida in, condimentum sit amet, nunc. Nam a nibh. Donec
+                suscipit eros. Nam mi. Proin viverra leo ut odio. Curabitur
+                malesuada. Vestibulum a velit eu ante scelerisque vulputate.
+            `
+        },
+        {
+            header: '2.一级条目',
+            content: `
+                Mauris mauris ante, blandit et, ultrices a, suscipit eget,
+                quam. Integer ut neque. Vivamus nisi metus, molestie vel,
+                gravida in, condimentum sit amet, nunc. Nam a nibh. Donec
+                suscipit eros. Nam mi. Proin viverra leo ut odio. Curabitur
+                malesuada. Vestibulum a velit eu ante scelerisque vulputate.
+            `
+        }
+    ];
+}
