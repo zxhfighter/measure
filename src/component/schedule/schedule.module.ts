3a6767c3cc792dcc4b1d5285fc195fa37da60454
yangxue <yangxue08@baidu.com>
2017-12-21 17:29:28 +0800

    格式化代码

--- src/component/schedule/schedule.module.ts
+++ src/component/schedule/schedule.module.ts
@@ -1,11 +1,11 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {ScheduleComponent} from './schedule';
-import {SelectModule} from '../select';   
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ScheduleComponent } from './schedule';
+import { SelectModule } from '../select';
 
 @NgModule({
     imports: [CommonModule, SelectModule],
     declarations: [ScheduleComponent],
     exports: [ScheduleComponent]
 })
-export class ScheduleModule {}
+export class ScheduleModule { }
