32d1ba72b68accf2f440fde7442fee69434e81ab
zxhfighter <369749456@qq.com>
2017-12-29 18:09:40 +0800

    modify sidebar

--- src/demo/app.component.ts
+++ src/demo/app.component.ts
@@ -11,48 +11,6 @@ export class AppComponent {
     title = 'app';
     isDocs = false;
 
-    menuTree: any = {
-        title: '组件列表',
-        expanded: true,
-        root: '全部组件',
-        tree: [
-            {
-                id: 1,
-                name: '输入组件',
-                selectable: false,
-                children: [
-                    {
-                        id: 1000,
-                        name: 'input',
-                        selectable: true
-                    },
-                    {
-                        id: 1001,
-                        name: 'textarea',
-                        selectable: true
-                    }
-                ]
-            },
-            {
-                id: 2,
-                name: '按钮组件',
-                selectable: false,
-                children: [
-                    {
-                        id: 1000,
-                        name: 'button',
-                        selectable: true
-                    },
-                    {
-                        id: 1001,
-                        name: 'button-group',
-                        selectable: true
-                    }
-                ]
-            }
-        ]
-    };
-
     constructor(private router: Router) {
         this.router.events.subscribe((event: any) => {
             if (event instanceof NavigationEnd) {
