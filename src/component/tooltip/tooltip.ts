81b8422f0dc41ba3f1af0786ce2acf022949785a
wangfengjiao <wangfengjiao01@baidu.com>
2018-01-05 10:30:14 +0800

    fix build error

--- src/component/tooltip/tooltip.ts
+++ src/component/tooltip/tooltip.ts
@@ -204,8 +204,7 @@ export class TooltipDirective implements OnInit, OnChanges, OnDestroy {
             .attachTo(this.el, this.tiplayerInstance, this.placement);
 
         this.positionStrategy = positionStrategy;
-        this.tiplayerInstance.positionStrategy = positionStrategy;
-            this.tiplayerInstance.needReposition.subscribe(
+        this.tiplayerInstance.needReposition.subscribe(
             () => this.overlayPositionService.updatePosition(positionStrategy)
         );
     }
